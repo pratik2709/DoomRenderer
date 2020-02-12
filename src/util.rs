7031d9c6393b037ddee4f57d17dfea93664c38cc
pratiksaxena <pratik2709@gmail.com>
2020-02-12 09:04:53 +0530

    completing the header function

--- src/util.rs
+++ src/util.rs
@@ -6,10 +6,11 @@ pub fn u8_to_u32(u8s: &[u8]) -> u32{
         // Each bit of the output is 0 if the corresponding bit of x
         // AND of y is 0, otherwise it's 1.
 
-    let p1 = u8s[0];
-    let p2 = u8s[1] << 8;
-    let p2 = u8s[2] << 16;
-    let p2 = u8s[3] << 24;
+    //type conversion
+    let p1 = u32::from(u8s[0]);
+    let p2 = u32::from(u8s[1]) << 8;
+    let p3 = u32::from(u8s[2]) << 16;
+    let p4 = u32::from(u8s[3])<< 24;
 
     p1 | p2 | p3 | p4
 
--- src/wad.rs
+++ src/wad.rs
@@ -3,18 +3,14 @@ use std::path::Path;
 use std::io::*;
 use std::str;
 
+#[derive(Debug)]
 pub enum WadType {
     IWAD,
     PWAD,
     WAD2,
 }
 
-//struct Header
-//{
-//    char WADType[5];
-//    uint32_t DirectoryCount;
-//    uint32_t DirectoryOffset;
-//};
+
 //
 //struct Directory
 //{
@@ -23,10 +19,11 @@ pub enum WadType {
 //    char LumpName[9];
 //};
 
+#[derive(Debug)]
 pub struct Header {
     wadType: WadType,
-    directoryCount: u32,
-    directoryOffset: u32,
+    directoryCount: usize,
+    directoryOffset: usize,
 }
 
 pub struct Directory {
@@ -36,7 +33,7 @@ pub struct Directory {
 }
 
 impl Header {
-    fn from_file(mut file: &File) -> bool {
+    fn from_file(mut file: &File) -> Header {
         println!("loading WAD file..");
         // move the cursor 0 bytes from the start of the file
         file.seek(SeekFrom::Start(0)).unwrap_or_else(|e|
@@ -56,9 +53,18 @@ impl Header {
             _ => panic!("could not read header.")
         };
 
+        //usize is pointer-sized,
+        // thus its actual size depends on the architecture
+        // your are compiling your program for.
+        let num_lumps: usize = u8_to_u32(&header_raw[4..8]) as usize;
+        let directory_size: usize = u8_to_u32(&header_raw[8..12]) as usize;
 
 
-        true
+        Header{
+            wadType,
+            directoryCount: num_lumps,
+            directoryOffset: directory_size,
+        }
     }
 }
 
@@ -76,6 +82,7 @@ impl Wad{
            panic!("unable to open th WAD file {}", e)
         });
         let header = Header::from_file(&wadFile);
+        println!("{:?}", header)
 
     }
 }
