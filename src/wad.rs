476c82c70ee6dd74c7f4c8f1d55317d7a145c686
pratiksaxena <pratik2709@gmail.com>
2020-02-16 21:03:56 +0530

    intermediate for loading vertex data

--- src/wad.rs
+++ src/wad.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::path::Path;
 use std::io::*;
 use std::str;
+use std::mem;
 
 impl Wad {
     pub fn from_path(path: &str) -> Wad {
@@ -50,24 +51,46 @@ impl Wad {
     }
 
     pub fn readVertexMapData(&self, mapName: String) -> bool {
-        let iMapIndex = self.findMapIndex(mapName).unwrap();
+        let iMapIndex = self.findMapIndex(mapName);
 
-        println!("{}", iMapIndex);
-//        match iMapIndex {
-//            None => false,
-//            mapName => {
-//                false
-////                iMapIndex += EMAPSLUMPSINDEX::eVERTEXES as usize;
-////                match self.directories[iMapIndex]{
-////
-////                }
-////                if self.directories[iMapIndex] != "VERTEXES" {
-////                    return false;
-////                }
-//            }
-//        };
+        match iMapIndex {
+            None => false,
+            mapName => {
+                let mut newIMapIndex = iMapIndex.unwrap();
+                let vertexString = String::from("VERTEXES");
+                newIMapIndex += EMAPSLUMPSINDEX::eVERTEXES as usize;
+                match &self.directories[newIMapIndex].lumpName{
+                    vertexString => {
+                        let iVertexSizeInBytes = mem::size_of::<Vertex>();
+
+                        //not understanding this
+                        let iVertexCount =
+                            self.directories[newIMapIndex].lumpSize/iVertexSizeInBytes;
+
+//                        for x in 0..iVertexCount{
+//
+//                        }
+
+                    }
+                }
+                false
+            }
+        };
 
 
         true
+    }
+
+    pub fn readVertexData(mut file: &File, offset: usize){
+        file.seek(SeekFrom::Start(offset as u64)).unwrap_or_else(|e|
+            panic!("unable to read directory data {}", e));
+
+        let mut raw_data:[u8;8] = [0;8];
+        file.read_exact(&mut raw_data)
+            .unwrap_or_else(|e|
+            panic!("unable to read lump data {}", e));
+        let xPosition = u8_to_u32(&raw_data[0..4]) as usize;
+        let yPosition = u8_to_u32(&raw_data[4..8]) as usize;
+
     }
 }
