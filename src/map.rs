3819f5e33ba1b01ef9c23134fa35c977173407f3
pratiksaxena <pratik2709@gmail.com>
2020-03-02 23:58:55 +0530

    render automap

--- src/map.rs
+++ src/map.rs
@@ -12,9 +12,14 @@ impl Map{
 
     pub fn addVertex(&mut self, vertex: Vertex){
         self.vertexes.push(vertex);
+
     }
 
     pub fn addLinedef(&mut self, linedef: LineDef){
         self.lineDefs.push(linedef);
+    }
+
+    pub fn renderAutoMap(){
+
     }
 }
