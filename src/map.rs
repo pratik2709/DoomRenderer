6c794b2a3817c86201348942473adb00fd1c4e63
pratiksaxena <pratik2709@gmail.com>
2020-03-04 23:42:21 +0530

    intermediate for map initializer

--- src/map.rs
+++ src/map.rs
@@ -1,11 +1,32 @@
 #[derive(Debug)]
 pub struct Map{
     name: String,
+    xMin: u32,
+    xMax: u32,
+    yMin: u32,
+    yMax: u32,
+    autoMapScalefactor: u32,
     vertexes: Vec<Vertex>,
     lineDefs: Vec<LineDef>
 }
 
 impl Map{
+
+    pub fn new(name: String, vertexes: Vec<Vertex>, lineDefs: Vec<LineDef>)
+        -> Map{
+
+        Map{
+            name,
+            xMax: std::u32::MAX,
+            xMin: std::u32::MIN,
+            yMax: std::u32::MAX,
+            yMin: std::u32::MIN,
+            autoMapScalefactor: 15,
+            vertexes,
+            lineDefs
+        }
+    }
+
     pub fn getName(&self) -> String{
         self.name.clone()
     }
