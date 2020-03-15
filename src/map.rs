71c1fbfd2d715568fb26807babde6ff9b39e04f3
pratiksaxena <pratik2709@gmail.com>
2020-03-15 17:00:16 +0530

    intermediate for adding canvas to map

--- src/map.rs
+++ src/map.rs
@@ -11,17 +11,24 @@ pub struct Map {
     things: Vec<Thing>,
     player: Player,
     iLumpIndex: Option<u32>,
-    nodes: Vec<Node>
-
+    nodes: Vec<Node>,
+    iRenderXSize: i16,
+    iRenderYSize: i16,
 }
 
 impl Map {
     pub fn new(name: String, vertexes: Vec<Vertex>,
                lineDefs: Vec<LineDef>, things: Vec<Thing>,
                nodes: Vec<Node>,
-               player: Player,
+               player: Player,canvas: sdl2::render::Canvas<sdl2::video::Window>
     )
                -> Map {
+        let iRender = canvas.logical_size();
+        let mut iRenderXSize = iRender.0 as i16;
+        let mut iRenderYSize = iRender.1 as i16;
+        iRenderXSize -= 1;
+        iRenderYSize -= 1;
+
         Map {
             name,
             xMax: std::i16::MIN,
@@ -34,7 +41,9 @@ impl Map {
             things,
             player,
             nodes,
-            iLumpIndex: None
+            iLumpIndex: None,
+            iRenderXSize,
+            iRenderYSize
         }
     }
 
@@ -66,52 +75,34 @@ impl Map {
         let iXShift = -self.xMin;
         let iYShift = -self.yMin;
 
+        self.renderAutoMapWalls(canvas);
 
-        let iRender = canvas.logical_size();
-        let mut iRenderXSize = iRender.0 as i16;
-        let mut iRenderYSize = iRender.1 as i16;
-
-        self.renderAutoMapWalls(canvas, iXShift, iYShift,
-                                iRenderXSize,
-                                iRenderYSize);
-
-        self.renderAutoMapPlayer(canvas, iXShift, iYShift,
-                                iRenderXSize,
-                                iRenderYSize);
+        self.renderAutoMapPlayer(canvas);
 
 
     }
 
-    pub fn renderAutoMapWalls(&mut self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-                              iXShift: i16, iYShift: i16, mut iRenderXSize: i16, mut iRenderYSize:
-                              i16) {
-        iRenderXSize -= 1;
-        iRenderYSize -= 1;
+    pub fn renderAutoMapWalls(&mut self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+
 
         canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
         for line in &self.lineDefs {
             let vStart = self.vertexes[line.startVertex as usize];
             let vEnd = self.vertexes[line.endVertex as usize];
-            let point1 = sdl2::rect::Point::new(((vStart.xPosition + iXShift) / self
-                .autoMapScaleFactor) as i32,
-                                                (iRenderYSize - (vStart.yPosition + iYShift) / self
-                                                    .autoMapScaleFactor) as i32);
+            let x1 = self.remapXToScreen(vStart.xPosition);
+            let y1 = self.remapYToScreen(vStart.yPosition);
+            let x2 = self.remapXToScreen(vEnd.xPosition);
+            let y2 = self.remapYToScreen(vEnd.yPosition);
+            let point1 = sdl2::rect::Point::new(x1, y1);
 
-            let point2 = sdl2::rect::Point::new(((vEnd.xPosition + iXShift) / self
-                .autoMapScaleFactor) as i32,
-                                                (iRenderYSize - (vEnd.yPosition + iYShift) / self
-                                                    .autoMapScaleFactor) as i32);
+            let point2 = sdl2::rect::Point::new(x2, y2);
 
             canvas.draw_line(point1, point2);
         }
     }
 
-    pub fn renderAutoMapPlayer(&mut self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-                               iXShift: i16, iYShift: i16, mut iRenderXSize: i16, mut iRenderYSize:
-                              i16) {
+    pub fn renderAutoMapPlayer(&mut self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
 
-        iRenderXSize -= 1;
-        iRenderYSize -= 1;
 
         canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
 
@@ -120,11 +111,8 @@ impl Map {
                                  (-1, 1), (0, 1), (1, 1)];
 
         for i in &direction{
-            let x = ((self.player.xPosition + iXShift)/ self
-                .autoMapScaleFactor + i.0) as i32;
-
-            let y = (iRenderYSize - ((self.player.yPosition + iYShift)/self.autoMapScaleFactor + i
-                .1)) as i32 ;
+            let x = self.remapXToScreen(self.player.xPosition) + i.0;
+            let y = self.remapYToScreen(self.player.yPosition) + i.1;
 
             let pp = sdl2::rect::Point::new(x,y);
             canvas.draw_point(pp);
@@ -156,5 +144,9 @@ impl Map {
 
     pub fn remapXToScreen(&self, xMapPosition: i16) -> i32{
         ((xMapPosition + (-self.xMin)) / self.autoMapScaleFactor) as i32
+    }
+
+    pub fn remapYToScreen(&self, yMapPosition: i16) -> i32{
+        (self.iRenderYSize - ((yMapPosition + (-self.yMin)) / self.autoMapScaleFactor)) as i32
     }
 }
